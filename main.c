55507a8d743eb716ecb80a1a4f710e60d1fb7e29
jlee0942 <55135113+jlee0942@users.noreply.github.com>
2019-10-08 15:30:56 +0900

    05

--- main.c
+++ main.c
@@ -4,14 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i=0, meter;
+	int i;
 	
-	while(i<3)
-	{
-	 meter =i*1609;
-	 printf("%d mile is %d meter.\n", i, meter);
-	 i++;
-	}
-
+	for(i=0;i<10; i++)
+		printf("Hello world!\n");
+		
 	return 0;
 }
